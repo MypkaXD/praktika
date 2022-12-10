5681b78ef882ca60f09b89ed9b13d84787ee1983
MypkaXD <85798586+MypkaXD@users.noreply.github.com>
2022-12-10 19:22:34 +0300

    added iterator and fixed constructor cop

--- praktika4.cpp
+++ praktika4.cpp
@@ -4,16 +4,28 @@
 #define size_t int
 
 template <class T>
-class Vector {
+class List {
 private:
-	size_t m_size = 0;
-	T* m_data = nullptr;
+	struct Node {
+		T m_elem = T(); // элемент 
+		Node* m_next = nullptr; // указатель на следующий элемент
+		Node(T elem, Node* next) : m_elem(elem), m_next(next) {}
+		Node(T elem) : m_elem(elem) {}
+		Node() {}
+		Node(const Node& other) {
+			m_elem = other.m_elem;
+			delete[] m_next;
+			m_next = new Node();
+			m_next = other.m_next;
+		}
+	};
+	Node* m_first = nullptr;
 public:
 	class iterator {
 	private:
-		T* m_ptr = nullptr;
+		Node* m_ptr = nullptr;
 	public:
-		iterator(T* ptr) : m_ptr(ptr) {}
+		iterator(Node* ptr) : m_ptr(ptr) {}
 		iterator(const iterator& other) : m_ptr(other.m_ptr) {}
 		~iterator() {
 			m_ptr = nullptr;
@@ -24,46 +36,22 @@ public:
 		}
 
 		iterator& operator++() {
-			m_ptr += 1;
+			m_ptr = m_ptr->m_next;
 			return *this;
 		}
-		iterator operator++(size_t) {
+		iterator operator++(int) {
 			iterator temp = m_ptr;
 			m_ptr += 1;
 			return temp;
 		}
-
-		iterator& operator--() {
-			m_ptr -= 1;
-			return *this;
-		}
-		iterator operator--(size_t) {
-			iterator temp = m_ptr;
-			m_ptr -= 1;
-			return temp;
-		}
-
 		iterator operator+(size_t n) { // двигает не на ++ а на больше
-			iterator temp = m_ptr + n;
+			iterator temp = m_ptr;
+			for (size_t count = 0; count < n; ++count)
+				temp = temp->m_next;
 			return temp;
 		}
-		iterator operator-(size_t n) { // двигает не на -- а на больше
-			iterator temp = m_ptr - n;
-			return temp;
-		}
-
 		T& operator*() {
 			return *m_ptr;
-		}
-		//bool operator!=(const iterator& other) {
-		//	return (m_ptr != other.m_ptr);
-		//}
-		//bool operator==(const iterator& other) {
-		//	return (m_ptr == other.m_ptr);
-		//}
-		bool operator<(const iterator& other) {
-			if (m_ptr < other.m_ptr) return true;
-			else return false;
 		}
 		friend bool operator!=(const iterator& first, const iterator& second) {
 			return (first.m_ptr != second.m_ptr);
@@ -71,69 +59,225 @@ public:
 		friend bool operator==(const iterator& first, const iterator& second) {
 			return !(second == first);
 		}
-		friend size_t operator-(const iterator& first, const iterator& second) { // найти расстояние между двумя инетаротами
-			return size_t(first.m_ptr - second.m_ptr);
-		}
 	};
-	Vector() {}
-	Vector(size_t size) {
-		if (size <= 0)
-			throw ("ERROR: size should be greaater than zero");
-		m_size = size;
-		m_data = new T[m_size];
-	}
-	Vector(const Vector& other) {
-		m_size = other.m_size;
-		delete[] m_data;
-		m_data = new T[m_size];
-		iterator i(begin());
-		iterator k(other.begin());
-		for (k; k != other.end(); ++k) {
-			i = k;
-			++i;
+	List() {}
+	explicit List(const size_t size) {
+		if (size == 0)
+			return;
+		if (size < 0)
+			throw ("ERROR: SIZE SHOULD BE GRERATER THAN ZERO");
+		m_first = new Node();
+		Node* temp = m_first;
+		for (size_t count = 1; count < size; ++count) {
+			temp->m_next = new Node();
+			temp = temp->m_next;
 		}
 	}
-	Vector(const std::initializer_list<T>& list) {
-		m_data = new T[list.size()];
-		size_t count = 0;
-		for (auto it = list.begin(); it < list.end(); ++it) {
-			m_data[count] = *it;
-			count++;
+	List(size_t size, T elem) {
+		if (size == 0)
+			return;
+		m_first = new Node(elem);
+		Node* temp = m_first;
+		for (size_t count = 1; count < size; count++) {
+			temp->m_next = new Node(elem);
+			temp = temp->m_next;
+		}
+	}
+	//List(const T& elem) {
+	//	m_first = new Node(elem);
+	//}
+	List(List<T>& other) {
+		delete[] m_first;
+		m_first = new Node(other.m_first->m_elem);
+		Node* temp = m_first;
+		Node* hope = other.m_first;
+		hope = hope->m_next;
+		iterator count = other.begin();
+		for (++count; count != other.end(); ++count) {
+			temp->m_next = new Node(hope->m_elem);
+			temp = temp->m_next;
+			hope = hope->m_next;
+		}
+	}
+	List(const std::initializer_list<T>& list) {
+		delete[] m_first;
+		m_first = new Node(*list.begin());
+		Node* temp = m_first;
+		for (auto it = list.begin() + 1; it < list.end(); ++it) {
+			temp->m_next = new Node(*it);
+			temp = temp->m_next;
+		}
+	}
+	~List() {
+		Node* temp = m_first;
+		while (temp) {
+			Node* temp2 = temp->m_next;
+			delete[] temp;
+			temp = temp2;
+		}
+	}
+	List& operator=(List& other) {
+		if (this == &other)
+			return *this;
+		delete[] m_first;
+		m_first = new Node(other.m_first->m_elem);
+		Node* temp1 = m_first;
+		Node* temp2 = other.m_first->m_next;
+		iterator count = other.begin();
+		for (++count; count != other.end(); ++count) {
+			temp1->m_next = new Node(temp2->m_elem);
+			temp1 = temp1->m_next;
+			temp2 = temp2->m_next;
+		}
+		return *this;
+	}
+	size_t size() {
+		Node* temp = m_first;
+		size_t lenght = 0;
+		for (iterator count = begin(); temp!=nullptr; ++count){
+			lenght++;
+			temp = temp->m_next;
+		}
+		return lenght;
+	}
+
+	void print() {
+		Node* temp = m_first;
+		for (iterator count = begin(); count != end(); ++count) {
+			std::cout << temp->m_elem << "\t";
+			temp = temp->m_next;
 		}
 	}
 
-	T& operator[](const size_t index) noexcept{
-		return m_data[index];
-	}
-
-	const iterator begin() const {
-		return iterator(m_data);
-	}
-	const iterator end() const {
-		return iterator(iterator(begin()) + m_size);
-	}
-
-
-	friend std::ostream& operator<<(std::ostream& out, const Vector& other) {
-		out << "[\t";
-		iterator k(other.begin());
-		for (k; k != other.end();++k) {
-			out << *k << "\t";
+	void flip() {
+		Node* temp = m_first;
+		Node* it = m_first;
+		Node* temp1 = nullptr;
+		while (it) {
+			temp = it->m_next;
+			it->m_next = temp1;
+			temp1 = it;
+			it = temp;
 		}
-		out << "]";
-		return out;
+		m_first = temp1;
+	}
+
+	void merge(List& other) {
+		if (size() + other.size() == 0) return;
+		Node* result = new Node(m_first->m_elem);
+		Node* temp = result;
+		Node* tempHop = m_first->m_next;
+		iterator count = begin();
+		for (++count; count != end(); ++count) {
+			temp->m_next = new Node(tempHop->m_elem);
+			temp = temp->m_next;
+			tempHop = tempHop->m_next;
+		}
+		tempHop = other.m_first;
+		for (iterator count = other.begin(); count != other.end(); ++count) {
+			temp->m_next = new Node(tempHop->m_elem);
+			temp = temp->m_next;
+			tempHop = tempHop->m_next;
+		}
+		m_first = result;
+	}
+
+	const bool empty() const noexcept {
+		return !size();
+	}
+
+	//void clear() {
+	//	Node* temp = m_first;
+	//	delete[] m_first;
+	//	for (size_t count 0; count < size(); ++count) {
+	//
+	//	}
+	//}
+
+	void pop_front() {
+		if (m_first == nullptr)
+			throw("ERROR: the list is emptry");
+		Node* temp = m_first->m_next;
+		delete[] m_first;
+		m_first = temp;
+	}
+
+	void push_front(const T& elem) {
+		Node* temp1 = new Node(elem);
+		Node* temp2 = m_first;
+		m_first = temp1;
+		m_first->m_next = temp2;
+	}
+
+	iterator begin() {
+		return iterator(m_first);
+	}
+	iterator end() {
+		Node* temp = m_first;
+		for (size_t count = 0; count < size(); ++count) {
+			temp = temp->m_next;
+		}
+		return iterator(temp);
 	}
 };
 
 int main() {
 
-	Vector<int> v({1,2,3,4,5});
-	Vector<int> v1(3);
-	Vector<int>::iterator count(v1.begin());
-	for (count; count < v1.end(); ++count)
-		*count = 1;
-	std::cout << v1;
+	List<int> l1({ 1,2,3 });
+	List<int> l2({ 45,432,12 });
+	l1.merge(l2);
+	l1.print();
+	/*
+	List<int> l1({ 1,2,3 });
+	l1.push_front(10);
+	l1.print();
+	std::cout << std::endl;
+	l1.flip();
+	l1.print();
+	std::cout << std::endl;
+	l1.pop_front();
+	l1.print();
+	std::cout << std::endl;
+	l1.flip();
+	l1.print();
+	std::cout << std::endl;
+	l1.push_front(10);
+	std::cout << std::endl;
+	l1.print();
+	*/
+	/*
+	
+	List<int> l2({ 5,4,56 });
 
+	List<int> l3;
+	l3 = l2;
+	l3.print();
+	l3 = l3;
+	l3.print();
+	std::cout << std::endl;
+	l1.merge(l2);
+	l1.print();
+	*/
+
+
+	/*
+	List<int> list1(10, 2);
+	list1.print();
+
+	std::cout << list1.size() << std::endl;
+
+	List<int> list2(14);
+	list2.print();
+	std::cout << list2.size() << std::endl;
+
+	List<int> list3(list1);
+	list3.print();
+	std::cout << list3.size() << std::endl;
+
+	List<int> list4({ 1,2,3 });
+	list4.print();
+	std::cout << list4.size() << std::endl;
+	*/
 
 	return 0;
 }
